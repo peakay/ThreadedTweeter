d809da8701b1ad7b6b1bf8498d2032e5a42a584c
Trent Stohrer <tstohrer23@gmail.com>
2018-09-27 21:48:11 -0400

    Fixed json for auth in config.py, made token generation less wordy, writes out token to json file. Still lacking correct directory in config.py. No key or token validation. Error handling needs work

--- threaded_tweeter/config.py
+++ threaded_tweeter/config.py
@@ -1,18 +1,24 @@
 # dynamically loaded config
 import twitter
+import json
+from auth import get_access_token
 
 # who needs fancy staging libraries
-STAGE = "DEV"
+STAGE = "TEST"
 
 with open('settings.json', 'r') as f:
     config = json.load(f)
 
-if not config[STAGE]['CREDS']['CONSUMER_KEY'] or not config[STAGE]['CREDS']['CONSUMER_SECRET']:
+if not config['THREADEDTWEETER'][STAGE]['CREDS']['CONSUMER_KEY'] or not config['THREADEDTWEETER'][STAGE]['CREDS']['CONSUMER_SECRET']:
     raise Exception('consumer_key pair not found, have you made a Twitter app yet?')
 
-TWITTER_CREDS = config[STAGE]['CREDS']
+TWITTER_CREDS = config['THREADEDTWEETER'][STAGE]['CREDS']
 
-if not config[STAGE]['ACCESS_TOKEN'] or not config[STAGE]['ACCESS_SECRET']:
-    # do auth process
-    # write new keys to settings.json
+if not TWITTER_CREDS['ACCESS_TOKEN'] or not TWITTER_CREDS['ACCESS_SECRET']:
+    token_key, token_secret = get_access_token.get_access_token(TWITTER_CREDS['CONSUMER_KEY'], TWITTER_CREDS['CONSUMER_SECRET']) 
+    TWITTER_CREDS['ACCESS_TOKEN'] = token_key
+    TWITTER_CREDS['ACCESS_SECRET'] = token_secret
+    with open('settinds.json', 'w') as f:
+        json.dump(config, f, indent = 4)
+    print (token_key, token_secret)
     pass
