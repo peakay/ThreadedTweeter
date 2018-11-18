41957c5182014ac6bc378c5bade105769fc6ef71
Trent Stohrer <tstohrer23@gmail.com>
2018-11-18 02:16:25 -0500

    Moved test input and solutions to files

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,7 +1,7 @@
 import pytest
 from threaded_tweeter.file_handler import load_thread_file, load_media_file
 
-
+#The I/O functions ar every simple, this just tests that they are working
 def test_io():
     unparsed_thread_str = load_thread_file("./test_data/sample_thread_1")
     assert len(unparsed_thread_str) == 67
