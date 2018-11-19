2e301be57bc5a6f8f9f4cd3d44c24209ef0e510b
Trent Stohrer <tstohrer23@gmail.com>
2018-11-18 19:58:12 -0500

    Added backend test

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,5 +5,5 @@ from threaded_tweeter.file_handler import load_thread_file, load_media_file
 def test_io():
     unparsed_thread_str = load_thread_file("./test_data/sample_thread_1")
     assert len(unparsed_thread_str) == 67
-    pic = load_media_file("./test_data/panda.jpg")
+    pic = load_media_file("panda.jpg")
     
